c7daadd50d2f34cf1ecf278ab10b0e008e27a897
brooksphilip <brooksphilip@mac.com>
2024-04-28 23:30:57 -0400

    updating

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update
+# Install some of my favorite tools every time my workspace boots
+sudo apt install -y vim
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform
